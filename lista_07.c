d66918cb6a61bd66dd82a9ab607586a83f455804
Diogo Souza <diogouba11@gmail.com>
2022-11-10 12:33:20 -0300

    lista finalizada

--- lista_07.c
+++ lista_07.c
@@ -5,9 +5,6 @@
 typedef struct sPessoa {
     char nome[50];
     char matricula[10];
-    int idade;
-    float altura;
-
 } PESSOA;
 
 void incializarLista (PESSOA **lista) {
@@ -36,35 +33,35 @@ void imprimirLista(PESSOA **lista){
 }
 
 int inserirInicio(PESSOA **lista, int elemento){
-    PESSOA *nocaCelula = criarCelula();
-    if(novaCelula == NULL){
+    PESSOA *nocaCelula = criarPessoa();
+    if(novaPessoa == NULL){
         printf("\nErro: Memoria cheia!");
         return 0;
     }
     if(listaVazia(lista)){
         return inserirFim(lista, elemento);
     }
-    novaCelula->info = elemento;
-    novaCelula->next = (*lista);
-    (*lista) = novaCelula;
+    novaPessoa->info = elemento;
+    novaPessoa->next = (*lista);
+    (*lista) = novaPessoa;
     return 1;
 }
 
 int inserirFim(PESSOA **lista, int elemento){
-    PESSOA *novaCelula;
+    PESSOA *novaPessoa;
     PESSOA *auxiliar;
 
-    novaCelula = criarCelula();
-    if(novaCelula == NULL){
+    novaPessoa = criarPessoa();
+    if(novaPessoa == NULL){
         printf("\nErro: Memoria cheia!");
         return 0;
     }
 
-    novaCelula->info = elemento;
-    novaCelula->next = NULL;
+    novaPessoa->info = elemento;
+    novaPessoa->next = NULL;
 
     if(listaVazia(lista)){
-        (*lista) = novaCelula;
+        (*lista) = novaPessoa;
         return 1;
     }
 
@@ -74,7 +71,7 @@ int inserirFim(PESSOA **lista, int elemento){
         auxiliar = auxiliar->next;
     }
 
-    auxiliar->next = novaCelula;
+    auxiliar->next = novaPessoa;
     return 1;
 }
 
@@ -97,7 +94,150 @@ PESSOA removerInicio(PESSOA **lista){
     return removido;
 }
 
+PESSOA removerFim(PESSOA **lista){
+    PESSOA *removida;
+    PESSOA *anterior;
 
+    PESSOA *anterior;
+    PESSOA removido = criarPessoa("", -1);
+
+    if(listaVazia(lista)){
+        printf("\nErro: Lista vazia!");
+        return removido;
+    }
+
+    if(auxiliar->next == NULL){
+        return removerInicio(lista);
+    }
+
+    removido = (*lista);
+
+    while(removida->next != NULL){
+        anterior = removida;
+        removida = removida->next;
+    }
+
+    removido = removida->info;
+    anterior->next = NULL;
+    free(removida);
+    return removido;
+}
+
+PESSOA *pesquisarMatricula(PESSOA **lista, int mat){
+    PESSOA *auxiliar;
+
+    if(listaVazia(lista)){
+        printf("\nErro: Lista vazia!");
+        return NULL;
+    }
+
+    auxiliar = (*lista);
+    while(auxiliar != NULL){
+        if(auxiliar->info.matricula == mat){
+            return auxiliar;
+        }
+        auxiliar = auxiliar->next;
+    }
+    return NULL;
+}
+
+PESSOA removerMatricula(PESSOA **Lista){
+    PESSOA *removida;
+    PESSOA *anterior;
+    PESSOA pRemover = criarPessoa("", -1);
+
+    if(listaVazia(lista)){
+        printf("\nErro: Lista vazia!");
+        return pRemover;
+    }
+
+    removida = pesquisarMatricula(lista, mat);
+    if(removida == NULL){
+        printf("\nErro: Matricula nao encontrada!");
+        return pRemover;
+    }
+
+    if(removida == (*lista)){
+        return removerInicio(lista);
+    }
+
+    pRemover = removida->info;
+
+    anterior = (*lista);
+    while(anterior->next != removida){
+        anterior = anterior->next;
+    }
+
+    anterior->next = removida->next;
+    free(removida);
+    return pRemover;
+}
+
+int main(){
+    PESSOA temp;
+    PESSOA *ptrlista, *tempPes;
+    inicializarLista(&ptrlista);
+
+    strcpy(temp.nome, "Jose");
+    temp.matricula = 1;
+
+    tempPes = pesquisarMatricula(&ptrlista, 2);
+    if(tempPes == NULL){
+        printf("\nMatricula nao encontrada!");
+    } else {
+        printf("\nMatricula encontrada!");
+    }
+
+    inserirFim(&ptrlista, temp);
+    imprimirLista(&ptrlista);
+
+    tempPes = pesquisarMatricula(&ptrlista, 2);
+    if(tempPes == NULL){
+        printf("\nMatricula nao encontrada!");
+    } else {
+        printf("\nMatricula encontrada!");
+    }
+
+    strcpy(temp.nome, "Maria");
+    temp.matricula = 2;
+
+    inserirFim(&ptrlista, temp);
+    imprimirLista(&ptrlista);
+
+    strcpy(temp.nome, "Joao");
+    temp.matricula = 4;
+
+    tempPes = pesquisarMatricula(&ptrlista, 2);
+    if(tempPes != NULL){
+        printf("\nMatricula nao encontrada!");
+    } else {
+        printf("\nMatricula encontrada!");
+    }
+
+    inserirInicio(&ptrlista, temp);
+    imprimirLista(&ptrlista);
+
+    temp = removerMatricula(&ptrlista, 4);
+    imprimirLista(&ptrlista);
+
+    liberarLista(&ptrlista);
+    imprimirLista(&ptrlista);
+
+    return 0;
+}
+
+int menu(){
+    printf("===============MENU================\n");
+    printf("0 - Sair\n");
+    printf("1 - Verificar se está vazia\n");
+    printf("2 - Inserir pessoa no fim\n");
+    printf("3 - Inserir pessoa no inicio\n");
+    printf("4 - Imprimir lista de pessoas\n");
+    printf("5 - Remover pessoa do inicio\n");
+    printf("6 - Remover pessoa do fim\n");
+    printf("7 - Pesquisar pessoa por matricula\n");
+    printf("8 - Remover pessoa por matricula\n");
+}
 
 
 
